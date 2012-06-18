a7f22410464bceef49e6d5f8c9c1eee248d5e8f3
Kevin McCarthy <me@kevinmccarthy.org>
2012-06-18 12:05:04 -1000

    Add all my old ml-class exercises in case I want to refer to them

---
+++ mlclass_exercises/mlclass-ex7/plotDataPoints.m
@@ -0,0 +1,17 @@
+function plotDataPoints(X, idx, K)
+%PLOTDATAPOINTS plots data points in X, coloring them so that those with the same
+%index assignments in idx have the same color
+%   PLOTDATAPOINTS(X, idx, K) plots data points in X, coloring them so that those 
+%   with the same index assignments in idx have the same color
+
+% Create palette
+%palette = hsv(K + 1);
+%colors = palette(idx, :);
+
+% Plot the data
+%scatter(X(:,1), X(:,2), 15, colors);
+
+colormap(hsv(K + 1)(1:end-1, :)); 
+scatter(X(:,1), X(:,2), 15, idx);
+
+end
